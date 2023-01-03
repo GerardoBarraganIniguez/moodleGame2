945b24cd016350fc27aa03002e35bf0a02bd97fb
Mario Vallejo <MarioGV320@hotmail.com>
2023-01-03 08:42:57 -0600

    Agregue la vista para crear tareas

---
+++ resources/views/assignments/create.blade.php
@@ -0,0 +1,28 @@
+@extends('layouts.app')
+
+@section('title')
+    Crear tareas
+@endsection
+
+@section('css')
+    rel="stylesheet" href="/css/table.css"
+@endsection
+
+@section('header')
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <a class="navbar-brand" href="#">
+    <img src="\img\a.png" alt="" class="custom-img">
+  </a>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link" href="#" style="padding-left: 1140px">Cerrar sesión</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+@endsection
+
+@section('content')
+     
+@endsection
