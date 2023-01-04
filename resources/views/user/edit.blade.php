b2648a782e7a005b260bcee953372af0a7168867
Mario Vallejo <MarioGV320@hotmail.com>
2023-01-04 17:51:52 -0600

    Agregue las vistas de edit para usuario y classroom

--- resources/views/user/edit.blade.php
+++ resources/views/user/edit.blade.php
@@ -4,10 +4,23 @@
     Editar Usuario <i>{{ $user->name }}</i>
 @endsection
 
+@section('css')
+    rel="stylesheet" href="/css/form.css"
+@endsection
+
 @section('content')
-    <form action="{{ route('users.update', $user) }}" method="POST" autocomplete="off">
-        @method('PATCH')
-        <x-user-form-body :roles="$roles" :user="$user"/>
-        <input type="submit" value="Actualizar">
-    </form>
+<div class="frame" style="padding-top: 15px" >
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <h5 style="padding-top: 15px">Edición de usuario: <i>{{ $user->name }}</i> </h5>
+          <form action="{{ route('users.update', $user) }}" method="POST" autocomplete="off">
+            @method('PATCH')
+            <x-user-form-body :roles="$roles" :user="$user"/>
+            <input type="submit" value="Actualizar">
+        </form>   
+        </div>
+      </div>
+    </div>
+</div>
 @endsection
