245d3b41cbcf720b1a1e3e9ee27a242e58ba4ad3
thedeivit <da.059@hotmail.com>
2023-01-05 02:37:39 -0600

    Actualizacion de los views de todos los .edit

--- resources/views/section/edit.blade.php
+++ resources/views/section/edit.blade.php
@@ -1,13 +1,25 @@
-@extends('layouts.app')
+@extends('layouts.create')
 
 @section('title')
-    Editar seccion <i>{{ $section->name }}</i>
+    Editar seccion {{ $section->name }}
+@endsection
+
+@section('css')
+    rel="stylesheet" href="/css/form.css"
 @endsection
 
 @section('content')
-    <form action="{{ route('sections.update',$section) }}" method="POST" autocomplete="off">
-        @method('PATCH')
-        <x-section-form-body :section="$section"/>
-        <input type="submit" value="Actualizar">
-    </form>
+<div class="frame" style="padding-top: 15px" >
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <h5 style="padding-top: 15px">Edición de sección: <i>{{ $section->name }}</i> </h5>
+          <form action="{{ route('sections.update',$section) }}" method="POST" autocomplete="off">
+            @method('PATCH')
+            <x-section-form-body :section="$section"/>
+        </form>
+        </div>
+      </div>
+    </div>
+</div>
 @endsection
--- resources/views/subject/edit.blade.php
+++ resources/views/subject/edit.blade.php
@@ -1,13 +1,25 @@
-@extends('layouts.app')
+@extends('layouts.create')
 
 @section('title')
-    Editar Materia <i>{{ $subject->name }}</i>
+    Editar Materia {{ $subject->name }}
+@endsection
+
+@section('css')
+    rel="stylesheet" href="/css/form.css"
 @endsection
 
 @section('content')
-    <form action="{{ route('subjects.update', $subject) }}" method="POST" autocomplete="off">
-        @method('PATCH')
-        <x-subject-form-body :subject="$subject"/>
-        <input type="submit" value="Actualizar">
-    </form>
+<div class="frame" style="padding-top: 15px" >
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <h5 style="padding-top: 15px">Edición de materia: <i>{{ $subject->name }}</i> </h5>
+          <form action="{{ route('subjects.update', $subject) }}" method="POST" autocomplete="off">
+            @method('PATCH')
+            <x-subject-form-body :subject="$subject"/>
+        </form>
+        </div>
+      </div>
+    </div>
+</div>
 @endsection
