8b645fe8d42695df3bcb739c246de0a872086229
Mario Vallejo <MarioGV320@hotmail.com>
2023-01-14 16:24:01 -0600

    Hice que la paginacióon en el main de student y teacher este bien colocado

--- resources/views/teacherUser/main.blade.php
+++ resources/views/teacherUser/main.blade.php
@@ -17,30 +17,32 @@
 @endsection
 
 @section('content')
-
-<div class="rectangle-container" style="display:flex; justify-content:center;">
-@foreach ($classrooms as $classroom) 
-    <div class="rectangle" style="width: 300px; height: 250px; margin: 15px; border: 2px solid black; background-color: white; border-radius: 4px;">
-        <div class="title w-100" style="background-color: lightgray; text-align: center; padding: 10px;">{{ $classroom->subject->name }} <br>{{ $classroom->section->name }}</div>
-        <div class="list" style="margin: 10px">
-          <ul>
-            <li>Tarea 1</li>
-            <li>Tarea 2</li>
-            <li>Tarea 3</li>
-          </ul>
+<div style="display:flex; flex-direction:column">
+    <div class="rectangle-container" style="display:flex; justify-content:center;">
+    @foreach ($classrooms as $classroom) 
+        <div class="rectangle" style="width: 300px; height: 250px; margin: 15px; border: 2px solid black; background-color: white; border-radius: 4px;">
+            <div class="title w-100" style="background-color: lightgray; text-align: center; padding: 10px;">{{ $classroom->subject->name }} <br>{{ $classroom->section->name }}</div>
+            <div class="list" style="margin: 10px">
+            <ul>
+                <li>Tarea 1</li>
+                <li>Tarea 2</li>
+                <li>Tarea 3</li>
+            </ul>
+            </div>
+            <div class="separation" style="border-top: 1px solid gray; padding-top: 10px; margin: 10px">
+                <li>Mis calificaciones</li>
+                <form action="{{ route('classrooms.destroy', [$classroom, auth()->user()]) }}" method="POST">
+                    @csrf
+                    @method('DELETE')
+                    <input type="submit" value="Abandonar Clase" class="btn btn-danger">
+                </form>
+            </div>
         </div>
-        <div class="separation" style="border-top: 1px solid gray; padding-top: 10px; margin: 10px">
-            <li>Mis calificaciones</li>
-            <form action="{{ route('classrooms.destroy', [$classroom, auth()->user()]) }}" method="POST">
-                @csrf
-                @method('DELETE')
-                <input type="submit" value="Abandonar Clase" class="btn btn-danger">
-            </form>
-        </div>
+    @endforeach
+    {{-- $classrooms->links('pagination::simple-bootstrap-5') >--}} 
     </div>
-@endforeach
-{{  $classrooms->links('pagination::simple-bootstrap-5')  }}
+    <div style="padding-left:41%">
+        {{  $classrooms->links('pagination::simple-bootstrap-5')  }}
+    </div>
 </div>
-
-
 @endsection
