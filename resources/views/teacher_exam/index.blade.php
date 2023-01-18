035ce800506474aabc51a968c23ea455cea5bf31
Mario Vallejo <MarioGV320@hotmail.com>
2023-01-18 08:24:32 -0600

    Agregue la division entre student y teacher para las lecciones y examenes, además de sus vistas

---
+++ resources/views/teacher_exam/index.blade.php
@@ -0,0 +1,46 @@
+@extends('layouts.create')
+
+@section('title')
+    Exámemes
+@endsection
+
+@section('css')
+    rel="stylesheet" href="/css/table.css"
+@endsection
+
+@section('content')
+  <div class="frame" style="padding-top: 15px">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-12">
+        <h5 style="padding-top: 15px">Mis exámenes</h5>
+        <br>
+        <table class="table">
+          <thead>
+            <tr>
+              <th>No. de exámen</th>
+              <th>Título del exámen</th>
+              <th>Acciones</th>
+            </tr>
+          </thead>
+          <tbody>
+                <tr>
+                    <td>Exámen 1</td>
+                    <td>Bases de Datos</td>
+                    <td><a href="#" class="btn btn-primary">Realizar exámen</a></td>
+                </tr>
+                <tr>
+                  <td>Exámen 2</td>
+                  <td>Diseño de una base de datos</td>
+                  <td><a href="#" class="btn btn-primary">Realizar exámen</a></td>
+              </tr>
+              <tr>
+                <td>Exámen 3</td>
+                <td>Creación de una base de datos</td>
+                <td><a href="#" class="btn btn-primary">Realizar exámen</a></td>
+            </tr>
+          </tbody>
+        </table>
+        <a href="#" class="btn btn-primary" >Crear nuevo exámen</a>
+      </div>
+@endsection
